a40dfde0e1f823d6e7087a5d4d156a8b5fa92428
kurs <kurs@DESKTOP-V3HDKPQ>
2023-03-23 09:20:51 +0100

    Add project files.

---
+++ P01NarzedzieWebClient/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P01NarzedzieWebClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //https://www.google.com/search?q=pogoda kraków
+            WebClient wc = new WebClient();
+            
+            int a = 4;
+
+            string dane = wc.DownloadString("https://www.google.com/search?q=pogoda kraków");
+
+            Console.WriteLine(dane);
+
+
+            File.WriteAllText("c:\\dane\\strona.html", dane);
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ P02ZadaniePogoda/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.WebRequestMethods;
+
+namespace P02ZadaniePogoda
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string szukanyZnak = "°";
+            string znakKoncowy = ">";
+
+            while (true)
+            {
+                Console.WriteLine("Podaj nazwe miasta");
+                string miasto = Console.ReadLine();
+                string adres = $"https://www.google.com/search?q=pogoda {miasto}";
+
+                WebClient wc = new WebClient();
+                string dane = wc.DownloadString(adres);
+
+                int indx = dane.IndexOf(szukanyZnak);
+                int aktualnaPozycja = indx;
+
+                while (dane.Substring(aktualnaPozycja, 1) != znakKoncowy)
+                    aktualnaPozycja--;
+
+                string wynik = dane.Substring(aktualnaPozycja + 1, indx - aktualnaPozycja + 1);
+
+                Console.WriteLine(wynik);
+            }
+            
+
+            Console.ReadKey();
+
+        }
+    }
+}
